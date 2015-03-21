2ba5eb8d7f627b8fb0a12c9f0f50cebc8aa40fcd
Ryan Fink <rfink@Ryans-MacBook-Pro.local>
2015-03-20 21:52:01 -0400

    Fix init script, test, add lint/specs/syntax per template

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,25 +5,12 @@
 #   include pentahokettle
 class liteide {
   $version = '27.1'
-  $url = "http://sourceforge.net/projects/liteide/files/X${version}/liteidex${version}.macosx.zip/download"
-  $tmpDest = '/tmp/liteide.zip'
-  $appName = '/tmp/LiteIDE.app'
-
-  exec { 'wget':
-    command  => "wget ${url} -O ${tmpDest}",
-    unless   => "test -f ${tmpDest} || test -f ${appName}",
-  }
-
-  exec { 'unzip':
-    command  => "unzip ${tmpDest}",
-    unless   => "test -f ${tmpDest} || test -f ${appName}",
-    require  => Exec['wget'],
-  }
+  $url = "http://sourceforge.net/projects/liteide/files/X${version}/liteidex${version}.macosx.zip"
 
   package { 'LiteIDE':
     ensure   => installed,
-    provider => appdmg,
-    source   => $appName,
-    require  => Exec['unzip'],
+    provider => compressed_app,
+    source   => $url,
+    #require  => Exec['liteide_wget'],
   }
 }
--- spec/classes/liteide_spec.rb
+++ spec/classes/liteide_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 
 describe 'liteide' do
   it do
-    should contain_class('liteide')
     should contain_package('LiteIDE').with({
-      :source   => '/tmp/LiteIDE.app',
-      :provider => 'appdmg',
+      :provider => 'compressed_app',
     })
   end
 end
